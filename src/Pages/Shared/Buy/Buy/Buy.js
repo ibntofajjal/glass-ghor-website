b0ff4936bed83ceeebc6dea7a9bbd9550514b580
ibntofajjal <joynal@bintofajjal.info>
2021-11-17 19:33:08 +0600

    added buy post api

--- src/Pages/Shared/Buy/Buy/Buy.js
+++ src/Pages/Shared/Buy/Buy/Buy.js
@@ -7,25 +7,73 @@ import Navbar from "../../Navbar/Navbar";
 import "./Buy.css";
 
 const Buy = () => {
+  const { user } = useAuth();
   const { glassID } = useParams();
   const [glass, setGlass] = useState({});
-  const { user } = useAuth();
+
+  const initialInfo = {
+    name: glass.name,
+    email: user.email,
+    price: glass.cost,
+    date: "",
+    phone: "",
+    address: "",
+    creditNumber: "",
+  };
+  const [buyingInfo, setBuyingInfo] = useState(initialInfo);
+
+  const handleOnBlur = (e) => {
+    const field = e.target.name;
+    const value = e.target.value;
+    const newInfo = { ...buyingInfo };
+    newInfo[field] = value;
+    console.log(newInfo);
+    setBuyingInfo(newInfo);
+
+    e.preventDefault();
+  };
+
+  const handleBuySubmit = (e) => {
+    // Collect Data
+    const order = {
+      ...buyingInfo,
+      name: glass.name,
+      email: user.email,
+      price: glass.cost,
+    };
+
+    // Send to server
+    fetch(`http://localhost:5000/orders`, {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(order),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        // console.log(data);
+        alert("Successfully Place Your Order :D");
+      });
+    e.preventDefault();
+  };
 
   useEffect(() => {
     fetch(`http://localhost:5000/singleProduct/${glassID}`)
       .then((res) => res.json())
       .then((data) => setGlass(data));
   }, []);
-  console.log(glass);
+
   const {
     register,
     handleSubmit,
-    watch,
     formState: { errors },
   } = useForm();
+
   const onSubmit = (data) => {
     console.log(data);
   };
+
   return (
     <div>
       {/* =========NAVBAR */}
@@ -47,40 +95,63 @@ const Buy = () => {
           </div>
           <div className="col col-lg-6 col-12 buy-form">
             <form onSubmit={handleSubmit(onSubmit)}>
-              <input disabled value={glass?.name} {...register("name")} />{" "}
+              <input
+                disabled
+                value={glass?.name}
+                //  {...register("name")}
+                name="productName"
+              />
               <br />
               <input
                 type="email"
                 disabled
                 value={user?.email}
-                {...register("email")}
+                // {...register("email")}
+                name="email"
               />
               <br />
               <input
                 type="number"
                 placeholder="Credit Card No."
                 required
-                {...register("creditNumber")}
+                name="creditNumber"
+                onBlur={handleOnBlur}
+                // {...register("creditNumber")}
               />
               <br />
-              <input type="date" {...register("date")} /> <br />
-              <input disabled value={glass?.cost} {...register("price")} />{" "}
+              <input
+                type="date"
+                name="date"
+                onBlur={handleOnBlur}
+                // {...register("date")}
+              />
+              <br />
+              <input
+                disabled
+                value={glass?.cost}
+                // {...register("price")}
+                name="price"
+              />
               <br />
               <input
                 placeholder="Address"
                 required
-                {...register("Address")}
-              />{" "}
+                onBlur={handleOnBlur}
+                // {...register("Address")}
+                name="address"
+              />
               <br />
               <input
                 type="number"
                 required
                 placeholder="Phone Number"
-                {...register("number")}
+                onBlur={handleOnBlur}
+                // {...register("number")}
+                name="phone"
               />
               <br />
               {errors.exampleRequired && <span>This field is required</span>}
-              <button>Submit</button>
+              <button onClick={handleBuySubmit}>Submit</button>
             </form>
           </div>
         </div>
